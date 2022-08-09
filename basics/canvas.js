5f5dce105aad2b717a65afea58150fe01f9ea0d7
Valentin <93974733+valentindush@users.noreply.github.com>
2022-08-09 23:29:37 +0200

    finished interraction

--- basics/canvas.js
+++ basics/canvas.js
@@ -7,15 +7,15 @@ ctx.fillRect(100,100,100,100)
 
 //Drawing a line
 //Start
-ctx.beginPath()
-ctx.moveTo(240, 140)
-ctx.lineTo(300,150)
-ctx.lineTo(149,200)
-ctx.lineTo(249,20)
-ctx.lineTo(249,200)
-ctx.stroke()
-//End
-ctx.beginPath()
+// ctx.beginPath()
+// ctx.moveTo(240, 140)
+// ctx.lineTo(300,150)
+// ctx.lineTo(149,200)
+// ctx.lineTo(249,20)
+// ctx.lineTo(249,200)
+// ctx.stroke()
+// //End
+// ctx.beginPath()
 
 //Drawing arcs/Cirlces
 // ctx.arc(300,300,50,Math.PI * 2,0,false)
@@ -48,6 +48,17 @@ const drawCircles = (n)=>{
 
 //Creating animation
 
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener('mousemove',(e)=>{
+    mouse.x = e.x
+    mouse.y = e.y
+})
+
+
 
 
 function Circle(x,y,dx,dy,radius){
@@ -56,12 +67,22 @@ function Circle(x,y,dx,dy,radius){
     this.dx = dx
     this.dy = dy
     this.radius = radius
+    this.minRadius = radius
+    this.colors = [
+        '#E3A614',
+        '#6F9FAD',
+        '#AD4154',
+        '#307691',
+        '#A2AD97',
+    ]
+    this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
 
     this.draw = ()=>{
         ctx.beginPath()
         ctx.arc(this.x,this.y,this.radius,Math.PI * 2,0,false)
-        ctx.strokeStyle = "blue"
-        ctx.stroke()
+        ctx.fillStyle = `${this.color}`
+        ctx.fill()
+ 
 
     }
 
@@ -76,6 +97,17 @@ function Circle(x,y,dx,dy,radius){
     
         this.x += this.dx
         this.y += this.dy
+
+        //Interraction
+
+        if(mouse.x - this.x < 60 && mouse.x - this.x > -60 
+            && mouse.y - this.y < 60 && mouse.y - this.y > -60){
+                if(this.radius < 60){
+                    this.radius += 1
+                }
+        }else if(this.radius > this.minRadius){
+            this.radius -=1
+        }
         this.draw()
     }
 
@@ -85,13 +117,13 @@ const circle = new Circle(200,200,5,5,30)
 
 let circles = []
 
-for(let i = 0; i <= 500; i++ ){
+for(let i = 0; i <= 800; i++ ){
     let x = Math.random() * innerWidth
     let y = Math.random() * innerHeight
     let dx = (Math.random() - 0.5)*3
     let dy = (Math.random() - 0.5)*3
 
-    let radius = 10
+    let radius = Math.random() * 6 + 6
     circles.push(new Circle(x,y,dx,dy,radius))
 
 }
@@ -106,3 +138,22 @@ const animate = ()=>{
 }
 
 animate()
+const init = ()=>{
+    circles = []
+    for(let i = 0; i <= 800; i++ ){
+        let x = Math.random() * innerWidth
+        let y = Math.random() * innerHeight
+        let dx = (Math.random() - 0.5)*3
+        let dy = (Math.random() - 0.5)*3
+    
+        let radius = Math.random() * 6 + 6
+        circles.push(new Circle(x,y,dx,dy,radius))
+    
+    }
+}
+window.addEventListener('resize',()=>{
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    init()
+})
