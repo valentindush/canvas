a4e46ac97a2ea214934e34c8d6964175d6c90842
Valentin <93974733+valentindush@users.noreply.github.com>
2022-08-09 20:18:32 +0200

    circles

--- basics/canvas.js
+++ basics/canvas.js
@@ -8,7 +8,35 @@ ctx.fillRect(100,100,100,100)
 //Drawing a line
 //Start
 ctx.beginPath()
-ctx.moveTo(200, 100)
+ctx.moveTo(240, 140)
 ctx.lineTo(300,150)
+ctx.lineTo(149,200)
+ctx.lineTo(249,20)
+ctx.lineTo(249,200)
 ctx.stroke()
 //End
+ctx.beginPath()
+
+//Drawing arcs/Cirlces
+ctx.arc(300,300,50,Math.PI * 2,0,false)
+ctx.strokeStyle = "blue"
+ctx.stroke()
+
+//Creating circls Randomly
+
+
+
+const drawCircles = (n)=>{
+
+    for(i = 0; i <= n; i++){
+        const x = Math.random() * window.innerWidth
+        const y = Math.random() * window.innerWidth
+        ctx.beginPath()
+
+        ctx.arc(x,y,50,Math.PI * 2,0,false)
+        ctx.strokeStyle = "blue"
+        ctx.stroke()
+    }
+}
+
+drawCircles(100)
